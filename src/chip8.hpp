569688134237c9db72eb50c7f0c577e538b3cdce
sathwikv2005 <sathwikv2005@gmail.com>
2026-09-03 15:48:24 +0530

    add chip8 class

--- src/chip8.hpp
+++ src/chip8.hpp
@@ -1,1 +1,14 @@
 #pragma once
+#include <array>
+#include <cstdint>
+#include <string>
+
+class Chip8 {
+   public:
+    Chip8();
+    void loadROM(const std::string& path);
+
+   private:
+    // 4KB RAM
+    std::array<uint8_t, 4096> memory{};
+};
