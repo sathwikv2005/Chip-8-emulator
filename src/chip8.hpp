bcc9412ca693a69893792a41356268c9e6d05fee
sathwikv2005 <sathwikv2005@gmail.com>
2026-09-04 11:29:33 +0530

    add pc and opcode

--- src/chip8.hpp
+++ src/chip8.hpp
@@ -11,4 +11,10 @@ class Chip8 {
    private:
     // 4KB RAM
     std::array<uint8_t, 4096> memory{};
+
+    // a chip-8 program starts from the memory address 0x200.
+    uint16_t pc{0x200};
+
+    // current opcode
+    uint16_t opcode{};
 };
