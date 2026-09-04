59152cfec4a6464043c852b33729211235711635
sathwikv2005 <sathwikv2005@gmail.com>
2026-09-04 14:24:57 +0530

    add chip-8 specs

--- src/chip8.hpp
+++ src/chip8.hpp
@@ -12,6 +12,18 @@ class Chip8 {
     // 4KB RAM
     std::array<uint8_t, 4096> memory{};
 
+    // variable registers
+    std::array<uint8_t, 16> v{};
+
+    std::array<uint16_t, 16> stack{};
+
+    // Each row has 64 pixels, hence each pixel is a bit in a uint64_t
+    // 64px*32px
+    std::array<uint64_t, 32> display{};
+
+    // index register
+    uint16_t I{};
+
     // a chip-8 program starts from the memory address 0x200.
     uint16_t pc{0x200};
 
