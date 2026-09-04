d4f461157877871dedf24dbfbfbfa04819d3feef
sathwikv2005 <sathwikv2005@gmail.com>
2026-09-04 16:53:22 +0530

    add renderer

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -1,5 +1,45 @@
 #include "Renderer.hpp"
 
-Renderer::Renderer(Chip8& chip8) : cpu(chip8) {}
+#include <stdexcept>
 
-void Renderer::render() {}
+Renderer::Renderer(Chip8& chip8) : cpu(chip8) {
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) throw std::runtime_error(SDL_GetError());
+
+    window =
+        SDL_CreateWindow("CHIP-8", SDL_WINDOWPOS_CENTERED,
+                         SDL_WINDOWPOS_CENTERED, 640, 320, SDL_WINDOW_SHOWN);
+
+    if (!window) throw std::runtime_error(SDL_GetError());
+
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    if (!renderer) throw std::runtime_error(SDL_GetError());
+}
+
+Renderer::~Renderer() {
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
+
+void Renderer::render() {
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+
+    const auto& display = cpu.getDisplay();
+
+    for (int y = 0; y < 32; ++y) {
+        for (int x = 0; x < 64; ++x) {
+            uint64_t mask = uint64_t{1} << (63 - x);
+
+            if (display[y] & mask) {
+                SDL_Rect pixel{x * 10, y * 10, 10, 10};
+
+                SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+                SDL_RenderFillRect(renderer, &pixel);
+            }
+        }
+    }
+
+    SDL_RenderPresent(renderer);
+}
--- src/renderer.hpp
+++ src/renderer.hpp
@@ -1,10 +1,18 @@
 #pragma
+#include <SDL2/SDL.h>
+
 #include "chip8.hpp"
+
 class Renderer {
    public:
     Renderer(Chip8& chip8);
+    ~Renderer();
+
     void render();
 
    private:
     Chip8& cpu;
+
+    SDL_Window* window{};
+    SDL_Renderer* renderer{};
 };
