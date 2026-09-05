e06ff3349e60e638342b0f288030a62f2473f6bc
sathwikv2005 <sathwikv2005@gmail.com>
2026-09-05 10:05:28 +0530

    boot IBM logo

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,23 @@
+#include <iostream>
+
 #include "chip8.hpp"
 #include "renderer.hpp"
 
-int main() {
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        std::cerr << "Usage: chip8 <rom>\n";
+        return 1;
+    }
+
     Chip8 chip8;
     Renderer renderer(chip8);
 
-    chip8.loadROM("../roms/IBM Logo.ch8");
+    chip8.loadROM(argv[1]);
 
-    // while (true) {
-    //     chip8.cycle();
-    //     renderer.render();
-    // }
+    while (true) {
+        chip8.cycle();
+        renderer.render();
+    }
 
     return 0;
 }
