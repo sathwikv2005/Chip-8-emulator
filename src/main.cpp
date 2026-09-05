209c263de4347e34557888d24a3006bae22c1c42
sathwikv2005 <sathwikv2005@gmail.com>
2026-09-05 10:29:42 +0530

    add cpu cycles

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,38 @@
+#include <bits/chrono.h>
+
 #include <iostream>
 
 #include "chip8.hpp"
 #include "renderer.hpp"
+
+#define CPU_CYCLES_PER_SEC 500
+
+void run(const char* path) {
+    Chip8 chip8;
+    Renderer renderer(chip8);
+
+    chip8.loadROM(path);
+
+    auto lastCycle = std::chrono::steady_clock::now();
+
+    // cpu cycles per second
+    const double cycleTime = 1.0 / CPU_CYCLES_PER_SEC;
+
+    while (true) {
+        auto now = std::chrono::steady_clock::now();
+        auto elapsed = std::chrono::duration<double>(now - lastCycle);
+        if (elapsed.count() >= cycleTime) {
+            chip8.cycle();
+
+            // add cycleTime instead of setting it to `now` to prevent timing
+            // drifts.
+            lastCycle +=
+                std::chrono::duration_cast<std::chrono::steady_clock::duration>(
+                    std::chrono::duration<double>(cycleTime));
+        }
+        renderer.render();
+    }
+}
 
 int main(int argc, char* argv[]) {
     if (argc != 2) {
@@ -9,15 +40,7 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    Chip8 chip8;
-    Renderer renderer(chip8);
-
-    chip8.loadROM(argv[1]);
-
-    while (true) {
-        chip8.cycle();
-        renderer.render();
-    }
+    run(argv[1]);
 
     return 0;
 }
